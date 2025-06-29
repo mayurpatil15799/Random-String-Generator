ae17b86070a3a78ca96762b8a74cb6aba0836d50
mayurpatil15799 <105695887+mayurpatil15799@users.noreply.github.com>
2025-06-29 18:09:50 +0530

    IAV Assignment

--- app/src/main/java/com/example/randomstring/MainActivity.kt
+++ app/src/main/java/com/example/randomstring/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.example.randomstring.data.RandomStringDatabase
 import com.example.randomstring.repository.RandomStringRepository
